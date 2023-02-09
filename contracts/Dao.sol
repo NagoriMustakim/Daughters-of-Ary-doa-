a28c5ce806715bcdc3311df9ea018d4aa60e1985
Mustakim <Mustakimkhan1111176@gmail.com>
2023-02-09 22:37:21 +0530

    doa-smaple

---
+++ contracts/Dao.sol
@@ -0,0 +1,103 @@
+pragma solidity ^0.8.0;
+
+contract DAO {
+    // Struct to store member information
+    struct Member {
+        uint256 nftType;
+        bool isCommon;
+        bool isRare;
+        bool isHero;
+        uint256 votingWeight;
+    }
+
+    // Struct to store funding application information
+    struct FundingApplication {
+        address applicant;
+        uint256 amount;
+        uint256 yesVotes;
+        uint256 noVotes;
+        uint256 status;
+        uint256 startTime;
+        uint256 endTime;
+    }
+
+    // Enum to define NFT types
+    enum NFTType {
+        Common,
+        Rare,
+        Hero
+    }
+
+    // Enum to define application status
+    enum ApplicationStatus {
+        Pending,
+        Approved,
+        Declined,
+        Funded
+    }
+
+    // Mapping to store member information
+    mapping (address => Member) public members;
+
+    // Mapping to store voting information
+    mapping (address => bool) public voting;
+
+    // Array to store funding applications
+    FundingApplication[] public fundingApplications;
+
+    // Public fund balance
+    uint256 public fundBalance;
+
+    // Function to add a member
+    function addMember(uint256 nftType) public {
+        Member storage member = members[msg.sender];
+        member.nftType = nftType;
+        switch (nftType) {
+            case uint256(NFTType.Common):
+                member.isCommon = true;
+                member.votingWeight = 1;
+                break;
+            case uint256(NFTType.Rare):
+                member.isRare = true;
+                member.votingWeight = 5;
+                break;
+            case uint256(NFTType.Hero):
+                member.isHero = true;
+                member.votingWeight = 25;
+                break;
+        }
+    }
+
+    // Function to submit a funding application
+    function submitFundingApplication(uint256 amount) public {
+        FundingApplication memory newApplication;
+        newApplication.applicant = msg.sender;
+        newApplication.amount = amount;
+        newApplication.yesVotes = 0;
+        newApplication.noVotes = 0;
+        newApplication.status = uint256(ApplicationStatus.Pending);
+        newApplication.startTime = now;
+        newApplication.endTime = now + 1 week;
+        fundingApplications.push(newApplication);
+    }
+
+    // Function to vote on a funding application
+    function vote(uint256 applicationIndex, bool isYes) public {
+        FundingApplication storage application = fundingApplications[applicationIndex];
+        Member storage member = members[msg.sender];
+        if (!voting[msg.sender]) {
+            if (isYes) {
+                application.yesVotes += member.votingWeight;
+            } else {
+                application.noVotes += member.votingWeight;
+            }
+            voting[msg.sender] = true;
+        }
+    }
+
+    // Function for a hero member to veto a funding application
+    function veto(uint256 applicationIndex) public {
+        FundingApplication storage application = fundingApplications[applicationIndex];
+        Member storage member = members[]
+    } 
+} 
