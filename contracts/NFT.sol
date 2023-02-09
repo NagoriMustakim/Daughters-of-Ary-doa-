f0ed838565890c33dcc2bbb1431e9cd8c91a5071
Mustakim <Mustakimkhan1111176@gmail.com>
2023-02-09 22:34:02 +0530

    erc721.sol

---
+++ contracts/NFT.sol
@@ -0,0 +1,116 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {Counters} from "@openzeppelin/contracts/utils/Counters.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+contract NFT is ERC721, Ownable, ReentrancyGuard, Pausable {
+    //event
+    event TransferTo(address indexed from, address indexed to, uint256 tokenid);
+
+    //state veraible
+    string private baseURI;
+    string private baseExtension = ".json";
+    address public founder;
+    uint256 private tokenCounter = 1;
+    bool private heroNFT;
+    //static prcing
+    uint256 public legendNFT = 1.54 ether;
+    uint256 public rareNFT = 0.31 ether;
+    uint256 public UncommonNFT = 0.062 ether;
+    uint256 public commonNFT = 0.012 ether;
+    constructor(address _founder, string memory _initBaseURI)
+        ERC721("HeroNFT", "HNFT")
+    {
+        founder = _founder;
+        baseURI = _initBaseURI;
+    }
+
+    //modifier
+    modifier HeroCounter() {
+        require(tokenCounter <= 25, "All hero nft is minted");
+        _;
+    }
+    modifier generalCounter() {
+        require(heroNFT, "1st Round is not started, yet")
+        require(tokenCounter > 25, "Hero nft need to mint first");
+        _;
+    }
+
+    function mintHero() public onlyOwner ReentrancyGuard whenNotPaused {
+        for (uint256 i = 0; i < 25; i++) {
+            _safeMint(msg.sender, i);
+        }
+    }
+
+    function transferToFounders()
+        public
+        onlyOwner
+        ReentrancyGuard
+        whenNotPaused
+    {
+        for (uint256 i = 0; i < 25; i++) {
+            safeTransferFrom(msg.sender, founder, i);
+            emit TransferTo(msg.sender, founder, i);
+        }
+        heroNFT = true;
+    }
+
+    //public function
+
+
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        Strings.toString(tokenId + 1),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
+
+    function pause() external whenNotPaused onlyOwner {
+        _pause();
+    }
+
+    function unpause() external whenPaused onlyOwner {
+        _unpause();
+    }
+
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
+
+    function setBaseExtension(string memory _newBaseExtension)
+        public
+        onlyOwner
+    {
+        baseExtension = _newBaseExtension;
+    }
+
+    function updatedFounderAddress(address _newfounderAddress)
+        public
+        onlyOwner
+    {
+        founder = _founder;
+    }
+}
